a26227872ce7afbe702fa223517ef29556597593
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-07 10:14:21 -0500

    Create database.tf

---
+++ database.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "luit_demo_db" {
+  allocated_storage    = 5
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "liut_demo_db"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
