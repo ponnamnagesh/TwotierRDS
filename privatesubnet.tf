a2a60fcb69d6ba04ec6f02e7b763d542f963486e
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-09 17:45:26 -0500

    Update privatesubnet.tf

--- privatesubnet.tf
+++ privatesubnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.luit_terraform_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "private subnet 1"
