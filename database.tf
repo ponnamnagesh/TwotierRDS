eb149004a1f17847eb3063e3791c573b808b00ee
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-09 17:25:32 -0500

    Update database.tf

--- database.tf
+++ database.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "luit_demo_db" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  db_name              = "liut_demo_db"
+  db_name              = "liutdemodb"
   username             = var.db_username
   password             = var.db_password
   parameter_group_name = "default.mysql5.7"
